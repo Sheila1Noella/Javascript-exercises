91e5321c23b7f6897e61a1dd596580acf98fa894
Sheila1Noella <asheilanoela@gmail.com>
2020-12-15 16:16:02 +0200

    Display countries by region and search

--- Assignments/Search countries/search_countries.js
+++ Assignments/Search countries/search_countries.js
@@ -6,6 +6,9 @@ function changemode() {
 const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
 const countriesEl = document.getElementById('countries');
+const filterbtn = document.getElementById('filter');
+const regionFilters = filterbtn.querySelectorAll('li');
+
 
 const searchCountries = async searchText => {
     const res = await fetch('https://restcountries.eu/rest/v2/all');
@@ -26,16 +29,16 @@ const searchCountries = async searchText => {
     outputHtml(matches);
 };
 const outputHtml = matches => {
-    if (matches.length > 0) {
-        const html = matches.map(match => `
-        <div class="card card-body mb1 form-group">
-        <h2 style="color:grey">${match.name}<span class="text-primary">
-        ${match.capital}</span></h2>
-        <small  style="color:grey">Capital city: ${match.capital}<br>Region: ${match.region}<br>Population: ${match.population}</small>
-        </div>`).join('');
-        // console.log(html);
-        matchList.innerHTML = html;
-    }
+    // if (matches.length > 0) {
+    //     const html = matches.map(match => `
+    //     <div class="card card-body mb1 form-group">
+    //     <h2 style="color:grey">${match.name}<span class="text-primary">
+    //     ${match.capital}</span></h2>
+    //     <small  style="color:grey">Capital city: ${match.capital}<br>Region: ${match.region}<br>Population: ${match.population}</small>
+    //     </div>`).join('');
+    //     // console.log(html);
+    //     matchList.innerHTML = html;
+    // }
 };
 search.addEventListener('input', () => searchCountries(search.value));
 
@@ -48,22 +51,60 @@ async function getCountries() {
 function displayCountries(countries1) {
     countriesEl.innerHTML = '';
     
-    // countries1.forEach(country => {
+    countries1.forEach(country => {
     const countryEl = document.createElement('div');
     countryEl.classList.add('card');
 
     countryEl.innerHTML = `
     <div>
-       <img src="https://restcountries.eu/data/deu.svg" style="width: 300px;height:200px;" alt="Germany">
+       <img src="${country.flag}" style="width:300px;height:200px" alt="Germany">
    </div>
    <div class="card-body">
-   <h2>Germany</h2>
-   <p><strong>Population: </strong>677766</p>
-   <p><strong>Region:</strong>Africa</p>
-   <p><strong>Capital city:</strong>kigali</p>
-   <p><strong>Language:</strong>Kinyarwanda</p>
+   <h2 class="country-name">${country.name}</h2>
+   <p><strong>Population: </strong>${country.population}</p>
+   <p class="country-region"><strong>Region: </strong>${country.region}</p>
+   <p><strong>Capital city: </strong>${country.capital}</p>
    </div>
    `;
     countriesEl.appendChild(countryEl);
-// });
+});
 }
+filterbtn.addEventListener('click', () => {
+    filterbtn.classList.toggle('open');
+});
+
+search.addEventListener('input', (e) =>{
+    const { value } = e.target;
+    // const val = e.target.value;
+    // console.log(val);
+    const countryName = document.querySelectorAll('.country-name');
+  countryName.forEach(name => {
+      console.log(name.innerText);
+      if(name.innerText.toLowerCase().includes(value.toLowerCase())){
+       name.parentElement.parentElement.style.display = 'block';   
+      }
+      else{
+        name.parentElement.parentElement.style.display = 'none';
+      }
+  }); 
+});
+
+regionFilters.forEach(filter => {
+    filter.addEventListener('click', () => {
+        const countryRegion = document.querySelectorAll('.country-region');
+        countryRegion.forEach(region => {
+            // console.log(region.innerText);
+            if(region.innerText.toLowerCase().includes(filter.innerText.toLowerCase())){
+             region.parentElement.parentElement.style.display = 'block';   
+            }
+            else{
+              region.parentElement.parentElement.style.display = 'none';
+            }
+        }); 
+    });
+});
+// regionFilters.foreach(filter => {
+//     filter.addEventListener('click', () => {
+//         console.log(filter.innerText);
+//     });
+// });
